5d1d329118538ca0ac271b83b9965103908303c4
Aman <amank422815@gmail.com>
2026-03-11 19:49:00 +0530

    data update

--- auth-backend/Routes/Route.js
+++ auth-backend/Routes/Route.js
@@ -49,61 +49,28 @@ router.post("/login", async (req, res) => {
   }
 });
 
+router.put("/update/:id", async (req, res)=>{
+  try {
+    const userid=req.params.id
+    const updateddata=req.body
+
+    const response=await model.findByIdAndUpdate(userid, updateddata,{
+      new:true,
+      runValidators:true
+    })
+    if(!response){
+     return res.status(404).json({message:"user not found"})
+    }
+    res.status(200).json({message:"data updated successfully", response})
+  } catch (error) {
+    console.log(error)
+    res.status(500).json({message:"Internal server error"})
+  }
+})
+
+
+
 module.exports = router;
 
-// exports.loginuser = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-//     const user = await usermodel.findOne({ email });
-//     if (!user) {
-//       return res.status(400).json({
-//         message: "user not found",
-//       });
-//     }
 
-//     const ismatch = await bcrypt.compare(password, user.password);
-//     if (!ismatch) {
-//       return res.status(400).json({
-//         message: "invalid password",
-//       });
-//     }
 
-//     const token = jwt.sign({ id: user._id }, "mysecretkey", {
-//       expiresIn: "10s",
-//     });
-
-//     res.json({
-//       message: "login successfull",
-//       token,
-//       user: {
-//         id: user._id,
-//         email: user.email,
-//       },
-//     });
-//   } catch (error) {
-//     console.log(error);
-//     res.status(500).json({
-//       message: "server internal error",
-//     });
-//   }
-// };
-
-// exports.updateruser = async(req,res)=>{
-//   try {
-//      const userid=usermodel.findById(req.params.id)
-//      const updateddata=req.body
-
-//      const response=await usermodel.findByIdAndUpdate(userid, updateddata,{
-//       new:true,
-//       runValidators:true
-//      })
-//      if(!response){
-//       res.status(404).json({message:"user not found"})
-//      }
-//     res.status(200).json({message:"data updated successfully", response})
-
-//   } catch (error) {
-//   console.log(error)
-//   res.status(500).json({message:"internal server error"})
-//   }
-// }
